197058a61eb6295756dcca14a16ea8f1e86c1102
Abicharan <abicharan.b@gmail.com>
2018-09-22 16:22:41 +0800

    Added directions for webpages

--- hello.py
+++ hello.py
@@ -10,21 +10,21 @@ def home_page():
 def bigdata_page():
     return render_template('bigdata.html', title = 'Big Data')
 
-#@app.route('/Services')
-#def service_page():
-#    return render_template('services.html')
+@app.route('/Services')
+def service_page():
+    return render_template('services.html', title = "Services")
  
-#@app.route('/Products')
-#def products_page():
-#    return render_template('Products.html')
+@app.route('/Products')
+def products_page():
+    return render_template('Products.html', title = "Products")
 
-#@app.route('/Projects')
-#def projects_page():
-#    return render_template('Projects.html')
+@app.route('/Projects')
+def projects_page():
+    return render_template('Projects.html', title = "Projects")
 
-#@app.route('/AboutUs')
-#def aboutUs_page():
-#    return render_template('aboutUs.html')
+@app.route('/AboutUs')
+def aboutUs_page():
+    return render_template('aboutUs.html', title = "About us")
 
 if __name__ == "__main__":
     app.run(debug=True)
