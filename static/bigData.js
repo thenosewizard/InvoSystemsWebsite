016461e0dca165ec8b44d1752a03ff619f039e7b
Abicharan <abicharan.b@gmail.com>
2018-09-28 18:02:42 +0800

    Added buttons (bigData)

--- static/bigData.js
+++ static/bigData.js
@@ -3,3 +3,12 @@
 $(document).ready( function () {
         $('#wholeSection').fadeIn(1100);
 })
+
+// Function that allows user to scroll to the top of the page
+$(document).ready( function () {
+    $('#scrollTop').on('click', function () {
+        $('html, body').animate({               
+            scrollTop: $("#scrollTo").offset().top  
+        },500);
+    });
+})
