d96e843bb88188285d3cf2d9fc7dccaf243b0267
Abicharan <abicharan.b@gmail.com>
2018-09-29 18:57:10 +0800

    animation for aboutpage [not stable]

--- static/aboutUs.js
+++ static/aboutUs.js
@@ -2,3 +2,18 @@
 $(document).ready( function () {
     $('#wholeAbout').fadeIn(1100);
 })
+
+
+$(document).ready(function () {
+    $('#aboutOne').on('click', function () {
+        $('#aboutTitleOne').fadeOut(50);
+        $('#textOne').fadeIn(2000); 
+    });      
+});
+
+$(document).ready(function () {
+    $('#textOne').on('click', function () {
+        $('#textOne').fadeOut();
+        $('#aboutTitleOne').fadeIn(2000);
+    });      
+});
